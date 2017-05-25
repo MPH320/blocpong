e79c2c4cc6ec45c15795d74cad3c7aaab7362763
Michelangelo <michelangelo.ph@gmail.com>
2017-05-25 12:29:09 -0700

    Explosion added

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ var ballHistory = [];
 var shakeTime = 120;
 var shakeTimer = shakeTime;
 var shake = false;
+var explosion = [];
 
 var coinFlip = function() {
     return Math.floor(Math.random() * 2);
@@ -112,9 +113,11 @@ var ballMovement = function(time) {
 	
 	//scored a point
 	if (ballX < 55){
+		startDoubleExplosion(ballX, ballY);
 		shake = true;
 		ballServe();
 	}else if (ballX > 245){
+		startDoubleExplosion(ballX, ballY);
 		shake = true;
 		ballServe();
 	}
@@ -183,6 +186,7 @@ var step = function(timestamp) {
     context.fillStyle = 'rgba(0, 0, 0, .05)';
   	context.fillRect(0, 0, canvas.width, canvas.height);
     render(timestamp - oldTimestamp);
+		updateAndDrawExplosion(timestamp - oldTimestamp);
     animate(step);
 		oldTimestamp = timestamp;
 };
@@ -192,4 +196,119 @@ window.onload = function() {
 	ballServe();
 }
 	
+function Particle ()
+	{
+		this.scale = 1.0;
+		this.x = 0;
+		this.y = 0;
+		this.radius = 20;
+		this.color = "#000";
+		this.velocityX = 0;
+		this.velocityY = 0;
+		this.scaleSpeed = 0.5;
+		this.useGravity = false;
+      
+		this.update = function(ms)
+		{
+			// shrinking
+          
+			this.scale -= this.scaleSpeed * ms / 1000.0;
+          
+			if (this.scale <= 0)
+			{
+              // particle is dead, remove it
+              removeFromArray(explosion, this);
+              
+			}
+			
+			// moving away from explosion center
+			this.x += this.velocityX * ms/1000.0;
+			this.y += this.velocityY * ms/1000.0;
+          
+          // and then later come downwards when our
+		  // gravity is added to it. We should add parameters 
+          // for the values that fake the gravity
+          if(this.useGravity) {
+              this.velocityY += Math.random()*4 +4;
+          }
+		};
+		
+		this.draw = function(context2D)
+		{
+			// translating the 2D context to the particle coordinates
+			context2D.save();
+			context2D.translate(this.x, this.y);
+			context2D.scale(this.scale, this.scale);
+			
+			// drawing a filled circle in the particle's local space
+			context2D.beginPath();
+			context2D.arc(0, 0, this.radius, 0, Math.PI*2, true);
+			//context2D.closePath();
+			
+			context2D.fillStyle = this.color;
+			context2D.fill();
+			
+			context2D.restore();
+		};
+	}
 
+function createExplosion(x, y, color)
+{
+		var minSize = 10;
+		var maxSize = 30;
+		var count = 10;
+		var minSpeed = 60.0;
+		var maxSpeed = 200.0;
+		var minScaleSpeed = 1.0;
+		var maxScaleSpeed = 4.0;
+		
+		for (var angle=0; angle<360; angle += Math.round(360/count))
+		{
+			var particle = new Particle();
+			
+			particle.x = x;
+			particle.y = y;
+			
+            // size of particle
+			particle.radius = random(1, 3);
+			
+			particle.color = color;
+			
+            // life time, the higher the value the faster particle 
+            // will die
+			particle.scaleSpeed = random(0.3, 0.5);
+          
+            // use gravity
+          particle.useGravity = true;
+			
+			var speed = random(minSpeed, maxSpeed);
+			
+			particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
+			particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
+			
+			explosion.push(particle);
+		}
+    
+	}
+
+function updateAndDrawExplosion(delta) {
+   for (var i = 0; i < explosion.length; i++) {
+	  var particle = explosion[i];
+			
+	  particle.update(delta);
+	  particle.draw(context);
+   }
+}
+
+function startDoubleExplosion(a, b) {
+	createExplosion(a, b, "#525252");
+	createExplosion(a, b, "#FFA318");
+}
+
+function removeFromArray(array, object) {
+    var idx = array.indexOf(object);
+    if (idx !== -1) {
+        array.splice(idx, 1);
+    }
+    return array;
+}
